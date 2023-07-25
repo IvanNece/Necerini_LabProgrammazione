0c2a146fe0f70002f158491b7aa47d21254c89ff
Ivan Necerini <ivan.necerini@gmail.com>
2023-07-25 15:09:29 +0200

    Creating clion project

---
+++ cmake-build-debug/CMakeFiles/Necerini_LabProgrammazione.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Necerini_LabProgrammazione.dir/main.cpp.obj"
+  "Necerini_LabProgrammazione.exe"
+  "Necerini_LabProgrammazione.exe.manifest"
+  "Necerini_LabProgrammazione.pdb"
+  "libNecerini_LabProgrammazione.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Necerini_LabProgrammazione.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
